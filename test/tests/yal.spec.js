bbeb6e2d1685f0d9135e37c313b97659d0a74707
Steffen <sateffen@gmail.com>
2015-10-19 21:57:53 +0200

    added new test as integration test

--- test/tests/yal.spec.js
+++ test/tests/yal.spec.js
@@ -1,10 +1,32 @@
 /* global describe, beforeEach, afterEach, it, expect */
 'use strict';
 
-var index = require('../../src/yal.js');
+var yal = require('../../src/yal.js');
 
-describe('index', function () {
+describe('integration', function () {
     it('should be an object', function () {
-        expect(index).to.be.an('object');
+        expect(yal).to.be.an('object');
+    });
+    
+    it('should call the computed when updating the observable', function () {
+        var executedComputed = false;
+        var observable = yal.observable(1);
+        var computed = yal.computed(function () {
+            executedComputed = true;
+            var toDouble = yal.unwrap(observable);
+            
+            return toDouble * 2;
+        });
+        
+        expect(executedComputed).to.equal(true);
+        expect(observable()).to.equal(1);
+        expect(computed()).to.equal(2);
+        
+        executedComputed = false;
+        observable(2);
+        
+        expect(executedComputed).to.equal(true);
+        expect(observable()).to.equal(2);
+        expect(computed()).to.equal(4);
     });
 });
